5f06ef4b091c0694d308ba5e5e7a2bac570e0dd7
Kulpot <kulpotwahu@gmail.com>
2024-05-14 04:19:57 +0800

    MainFrame.cpp->
    MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title)
    {
     wxPanel* panel = new wxPanel(this);
     std::vector<wxButton> buttons;
    
     for (int i = 0; i < 6; i++)
     {
      wxString label = wxString::Format("Button %d", i + 1);
      wxButton* button = new wxButton(panel, wxID_ANY, label, wxDefaultPosition, wxSize(300, 100));

--- wxGridSizer/MainFrame.cpp
+++ wxGridSizer/MainFrame.cpp
@@ -28,6 +28,7 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title)
 	for (int i = 0; i < 6; i++)
 	{
 		wxString label = wxString::Format("Button %d", i + 1);
+		wxButton* button = new wxButton(panel, wxID_ANY, label, wxDefaultPosition, wxSize(300, 100));
 
 	}
 }
