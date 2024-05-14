66dcf8b02072154883fd3594da45672ee811705d
Kulpot <kulpotwahu@gmail.com>
2024-05-15 04:48:29 +0800

    MainFrame.cpp->
     //for (int i = 0; i < 6; i++)
     for (int i = 0; i < 7; i++)
     {
      wxString label = wxString::Format("Button %d", i + 1);
      wxButton* button = new wxButton(panel, wxID_ANY, label, wxDefaultPosition, wxSize(300, 100));
      buttons.push_back(button);
     }
    
     //wxGridSizer* gridSizer = new wxGridSizer(3, 2, wxSize(0, 0));
     wxGridSizer* gridSizer = new wxGridSizer(0, 2, wxSize(0, 0));

--- wxGridSizer/MainFrame.cpp
+++ wxGridSizer/MainFrame.cpp
@@ -25,14 +25,16 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title)
 	wxPanel* panel = new wxPanel(this);
 	std::vector<wxButton> buttons;
 
-	for (int i = 0; i < 6; i++)
+	//for (int i = 0; i < 6; i++)
+	for (int i = 0; i < 7; i++)
 	{
 		wxString label = wxString::Format("Button %d", i + 1);
 		wxButton* button = new wxButton(panel, wxID_ANY, label, wxDefaultPosition, wxSize(300, 100));
 		buttons.push_back(button);
 	}
 
-	wxGridSizer* gridSizer = new wxGridSizer(3, 2, wxSize(0, 0));
+	//wxGridSizer* gridSizer = new wxGridSizer(3, 2, wxSize(0, 0));
+	wxGridSizer* gridSizer = new wxGridSizer(0, 2, wxSize(0, 0));
 
 	for (wxButton* button : buttons) {
 		gridSizer->Add(button);
